83cb9f5ffaac7f7db3cbc1c0222519907f91ac0f
Jan Pilgenröder <pille@mac.com>
2017-08-28 17:48:31 +0200

    ui tweaks

--- imports/client/ui/web/DisplayResult.vue
+++ imports/client/ui/web/DisplayResult.vue
@@ -5,20 +5,21 @@
   h4 Musterlösung:
   katex(v-bind:tex="data.solutionTeX")
   h4 Auswertung:
-  .result-header.pass(v-if="data.result.pass") Das Ergebnis ist korrekt:
-  .result-header.fail(v-else) Das Ergebnis ist nicht korrekt:
-  .fail-text(v-for="text in data.result.passTextsRequired" key="text")
-    icon.icon.pass(name="check")
-    |{{text}}
-  .fail-text(v-for="text in data.result.passTextsOptional" key="text")
-    icon.icon.pass(name="check")
-    |{{text}}
-  .fail-text(v-for="text in data.result.failTextsRequired" key="text")
-    icon.icon.fail(name="times")
-    |{{text}}
-  .fail-text(v-for="text in data.result.failTextsOptional" key="text")
-    icon.icon.warn(name="times")
-    |{{text}}
+  .box
+    .result-header.pass(v-if="data.result.pass") Das Ergebnis ist korrekt:
+    .result-header.fail(v-else) Das Ergebnis ist nicht korrekt:
+    .fail-text(v-for="text in data.result.passTextsRequired" key="text")
+      icon.icon.pass(name="check")
+      |{{text}}
+    .fail-text(v-for="text in data.result.passTextsOptional" key="text")
+      icon.icon.pass(name="check")
+      |{{text}}
+    .fail-text(v-for="text in data.result.failTextsRequired" key="text")
+      icon.icon.fail(name="times")
+      |{{text}}
+    .fail-text(v-for="text in data.result.failTextsOptional" key="text")
+      icon.icon.warn(name="times")
+      |{{text}}
 </template>
 
 <script lang="coffee">
@@ -29,27 +30,31 @@ return
 </script>
 
 <style scoped lang="sass">
+.box
+  border : 1px solid grey
+  box-shadow : 2px 2px 2px silver
+  margin-top : 10px
 .result-container
+  margin-top : 1em
   margin-bottom : 2em
 .result-header
   font-size : 1.2em
   color : white
   padding : 10px
-  margin-top : 10px
   &.fail
-    background-color : red
+    background-color : #FF4949
   &.pass
-    background-color : green
+    background-color : #13CE66
 .fail-text
-  padding : 10px 10px 0px 20px
+  padding : 10px 10px 10px 20px
 .icon
   width : 1.3em
   height : 1.3em
   margin-right : 20px
   &.fail
-    color : red
+    color : #FF4949
   &.pass
-    color : green
+    color : #13CE66
   &.warn
-    color : orange
+    color : #F7BA2A
 </style>
