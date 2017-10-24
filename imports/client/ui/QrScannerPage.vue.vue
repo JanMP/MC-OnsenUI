ce60e1e838d9fb7ba63c7417cfc756a369b9c3ce
Jan Pilgenröder <pille@mac.com>
2017-10-24 16:47:47 +0200

    tweaks

--- imports/client/ui/QrScannerPage.vue.vue
+++ imports/client/ui/QrScannerPage.vue.vue
@@ -1,9 +1,8 @@
 <template lang="jade">
 v-ons-page(modifier="full_bg")
   custom-toolbar(title="Test")
-  p
-  v-ons-button(@click="getCode") get QR Code
   div(v-html="qrCodeSVG")
+  v-ons-button(@click="getCode" modifier="large") get QR Code
 </template>
 
 <script lang="coffee">
@@ -20,28 +19,19 @@ return
       else
         onSuccess = (text) -> alert text
         onError = (err) -> console.log err
+        options =
+          flash : "off"
+          drawSight : false
         try
           cloudSky.zBar.scan {}, onSuccess, onError
         catch error
           @error = error
-    QrCode : ->
-      user = @$store.state.auth.user
-      unless user._id?
-        alert "User ist nicht angemeldet."
-        return
-      code = JSON.stringify
-        id : user._id
-        username : user.username
-        fullName :
-          if user?.profile?.firstName and user?.profile?.lastName
-            "#{user.profile.firstName} #{user.profile.lastName}"
-      @svg = qr.imageSync code, type : "svg"
   computed :
     scanningString : -> if @scanning then "on" else "off"
     isCordova : -> Meteor.isCordova
     qrCodeSVG : ->
       user = @$store.state.auth.user
-      code = if user._id?
+      code = if user?._id?
         JSON.stringify
           id : user._id
           username : user.username
@@ -49,7 +39,7 @@ return
             if user?.profile?.firstName and user?.profile?.lastName
               "#{user.profile.firstName} #{user.profile.lastName}"
       else
-        ""
+        "fnord"
       qr.imageSync code, type : "svg"
 </script>
 
